335c27ee7ea404e803b7c98af2f8c42cf6112382
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-11-12 21:02:06 +0100

    Added gstreamer deps

--- flake.nix
+++ flake.nix
@@ -48,6 +48,17 @@
             go
             gopls
 
+            pkg-config
+            gst_all_1.gstreamer
+            gst_all_1.gst-plugins-base
+            gst_all_1.gst-plugins-good
+            gst_all_1.gst-plugins-bad
+            gst_all_1.gst-plugins-ugly
+            gst_all_1.gst-libav
+            gst_all_1.gst-plugins-rs
+            glib-networking
+            openssl
+
             tools.publishVersion
           ];
         };
